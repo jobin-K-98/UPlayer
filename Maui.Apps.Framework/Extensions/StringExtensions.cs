51954c45db9080a9de2434209230b444f1647d0d
jobin-K-98 <122489680+jobin-K-98@users.noreply.github.com>
2023-07-06 21:08:52 -0400

    Create StringExtensions.cs
    created StringExtensions.cs in the Extensions folder for handling the uri.

---
+++ Maui.Apps.Framework/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+
+using System.Text.RegularExpressions;
+
+namespace Maui.Apps.Framework.Extensions;
+
+public static class StringExtensions
+{
+    public static string CleanCacheKey(this string uri) =>
+        Regex.Replace((new Regex("[\\~#%&*{}/:<>?|\"-]")).Replace(uri, " "), @"\s+", "_");
+
+}
+
