c3f3bd08494bb210f1ccbfbdb862c43d5b133b6d
Sri hari Sreenivasan <122223722+Srihari0911@users.noreply.github.com>
2023-07-12 21:34:01 -0400

    Update GlobalUsings.cs

--- Maui.Apps.Framework/GlobalUsings.cs
+++ Maui.Apps.Framework/GlobalUsings.cs
@@ -1,5 +1,9 @@
 global using MonkeyCache;
 global using System.Text;
+global using System.Text.RegularExpressions;
 global using System.Text.Json;
 global using Maui.Apps.Framework.Exceptions;
 global using Maui.Apps.Framework.Extensions;
+global using System.Collections.ObjectModel;
+global using System.Globalization;
+global using CommunityToolkit.Mvvm.ComponentModel;
