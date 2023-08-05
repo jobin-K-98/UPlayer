67c6e8d71fb6406d727ad12bbacee3e93c726a1a
Sri hari Sreenivasan <122223722+Srihari0911@users.noreply.github.com>
2023-08-05 19:12:05 -0400

    Created IApiService.cs

---
+++ UPlayer/Iservices/IApiService.cs
@@ -0,0 +1,9 @@
+namespace MauiTubePlayer.IServices;
+
+public interface IApiService
+{
+    Task<VideoSearchResult> SearchVideos(string searchQuery, string nextPageToken = "");
+    Task<ChannelSearchResult> GetChannels(string channelIDs);
+    Task<YoutubeVideoDetail> GetVideoDetails(string videoID);
+    Task<CommentsSearchResult> GetComments(string videoID);
+}
